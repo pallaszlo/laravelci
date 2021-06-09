9176cb0458ef5c17b5145f300e98d0e9a5d972b1
palci27 <pal.laszlo@yahoo.com>
2021-06-09 10:08:20 +0300

    testing

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -16,8 +16,34 @@ class ExampleTest extends DuskTestCase
     public function testBasicExample()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
+            $browser->visit('http://laravelci.test/')
                     ->assertSee('Laravel');
+        });
+    }
+
+    public function testExample()
+    {
+        $this->browse(function ($browser) {
+            //We'll test the register feature here
+            $browser->visit('http://laravelci.test/')
+                    ->clickLink('Register')
+                    ->value('#name', 'Samson') 
+                    ->value('#email', 'samson@test.com')
+                    ->value('#password', '00000000')
+                    ->value('#password_confirmation', '00000000')
+                    ->click('button[type="submit"]') 
+
+            //We'll test the login feature here
+                    ->press('Samson');
+                    if ($browser->seeLink('Log Out')) {
+                        $browser->clickLink('Log Out')
+
+                        ->clickLink('Login')
+                        ->value('#email', 'samson@test.com')
+                        ->value('#password', '00000000')
+                        ->click('button[type="submit"]');
+                    }
+
         });
     }
 }
---
+++ tests/Feature/RegistrationTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class RegistrationTest extends TestCase
+{
+
+    /**
+     * The registration form can be displayed.
+     *
+     * @return void
+     */
+    public function testRegisterFormDisplayed()
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A valid user can be registered.
+     *
+     * @return void
+     */
+    public function testRegistersAValidUser()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post('register', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ]);
+
+        $response->assertStatus(302);
+
+        //$this->assertAuthenticated();
+    }
+
+    /**
+     * An invalid user is not registered.
+     *
+     * @return void
+     */
+    public function testDoesNotRegisterAnInvalidUser()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post('register', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => 'password',
+            'password_confirmation' => 'invalid'
+        ]);
+
+        $response->assertSessionHasErrors();
+
+        $this->assertGuest();
+    }
+
+}
